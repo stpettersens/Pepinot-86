8db154bbdf6a55e99d7d5f6b16a6cb75c686cdb4
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2012-01-08 19:01:01 +0000

    ...

--- scripts/pepinotx.js
+++ scripts/pepinotx.js
@@ -1,7 +1,13 @@
 /*
 Main script functions for Pepinot 86 website.
+Depends on jQuery library.
 */
-function doAll()
+
+function addLikeButtons()
 {
-	
+	var fbLike = '<div class="fb-like" data-href="http://stpettersens.github.com/Pepinot-86/"'
+	+ 'data-send="false" data-width="450" data-show-faces="false" data-font="verdana"></div>';
+	var gPlusOneLike = '<g:plusone></g:plusone>';
+	$('div#like').append(gPlusOneLike + fbLike);
 }
+addLikeButtons();
