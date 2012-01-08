355698763ef9dd8f6aa94ee063c70862d54ed30c
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2012-01-08 18:49:51 +0000

    ...

--- scripts/facebook-sdk.js
+++ scripts/facebook-sdk.js
@@ -1,7 +1,7 @@
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '214282278637686', // App ID
-      channelUrl : '//stpettersens.github.com/Pepinot-86/channel.html', // Channel File
+      channelUrl : '//stpettersens.github.com/Pepinot-86/__channel__.html', // Channel File
       status     : true, // check login status
       cookie     : true, // enable cookies to allow the server to access the session
       xfbml      : true  // parse XFBML
