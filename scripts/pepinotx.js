73b1709a835b4055fa460c62fb80e3420e07b55c
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2012-01-08 20:41:25 +0000

    ...

--- scripts/pepinotx.js
+++ scripts/pepinotx.js
@@ -9,5 +9,9 @@ function addLikeButtons()
 	+ 'data-send="false" data-width="450" data-show-faces="false" data-font="verdana"></div>';
 	var gPlusOneLike = '<g:plusone></g:plusone>';
 	$('div#like').append(gPlusOneLike + fbLike);
+	/* Facebook Like button for some reason does not work in Chrome,
+	adjust so that Google +1 button is further to the left if in Chrome. 
+	Neither button works in Opera. They will just not display. */
+	if($.browser.webkit) $('div#like').css('margin-left', '5em');
 }
 addLikeButtons();
